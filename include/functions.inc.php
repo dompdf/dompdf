3d10eb2150d4fd7c26749080ad453551d7865ebe
flaviencrochard <flavien.crochard@gmail.com>
2010-03-26 14:47:05 +0000

    Correct resolving images. And change url of the css file in remote.html
    git-svn-id: http://dompdf.googlecode.com/svn/trunk/dompdf@237 8e70de24-3a84-11de-b438-597f59cd7555

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -131,10 +131,11 @@ function build_url($protocol, $host, $base_path, $url) {
     //drive: followed by a relative path would be a drive specific default folder.
     //not known in php app code, treat as abs path
     //($url[1] !== ':' || ($url[2]!=='\\' && $url[2]!=='/'))
+    $ret .= realpath($base_path);
     if ($url[0] !== '/' && (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN' || ($url[0] !== '\\' && $url[1] !== ':'))) {
       // For rel path and local acess we ignore the host, and run the path through realpath()
-      $ret .= realpath($base_path).'/';
-    }
+      $ret .= '/';
+	}
     $ret .= $url;
     $ret = preg_replace("/\?(.*)$/", "", $ret);
     return $ret;
--- include/style.cls.php
+++ include/style.cls.php
@@ -1287,26 +1287,7 @@ class Style {
     if ( mb_strpos($val, "url") === false ) {
       $path = "none"; //Don't resolve no image -> otherwise would prefix path and no longer recognize as none
     } else {
-      $val = preg_replace("/url\(['\"]?([^'\")]+)['\"]?\)/","\\1", trim($val));
-
-      // Resolve the url now in the context of the current stylesheet
-      $parsed_url = explode_url($val);
-      if ( $parsed_url["protocol"] == "" && $this->_stylesheet->get_protocol() == "" ) {
-        if ($parsed_url["path"][0] === '/' || $parsed_url["path"][0] === '\\' ) {
-          $path = $_SERVER["DOCUMENT_ROOT"].'/';
-        } else {
-          $path = $this->_stylesheet->get_base_path();
-        }
-        $path .= $parsed_url["path"] . $parsed_url["file"];
-        $path = realpath($path);
-        // If realpath returns FALSE then specifically state that there is no background image
-        if (!$path) { $path = 'none'; }
-      } else {
-        $path = build_url($this->_stylesheet->get_protocol(),
-                          $this->_stylesheet->get_host(),
-                          $this->_stylesheet->get_base_path(),
-                          $val);
-      }
+      $path = preg_replace("/url\(['\"]?([^'\")]+)['\"]?\)/","\\1", trim($val));
     }
     if ($DEBUGCSS) {
       print "<pre>[_image\n";
