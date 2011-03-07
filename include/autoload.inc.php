f2ffd7c3f7bd9a157df57d1be19236780386f6c9
PhenX <phenxdesign@gmail.com>
2011-03-07 20:14:45 +0000

    Added a file for the autoloader Fixed compatibility with the Symfony framework autoloader with a DOMPDF_AUTOLOAD_PREPEND constant, false by default Added missing svn:keywords
    git-svn-id: http://dompdf.googlecode.com/svn/trunk/dompdf@371 8e70de24-3a84-11de-b438-597f59cd7555

---
+++ include/autoload.inc.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * DOMPDF - PHP5 HTML to PDF renderer
+ *
+ * File: $RCSfile: autoload.inc.php,v $
+ * Created on: 2011-03-07
+ *
+ * Copyright (c) 2004 - Benj Carson <benjcarson@digitaljunkies.ca>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library in the file LICENSE.LGPL; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA
+ *
+ * Alternatively, you may distribute this software under the terms of the
+ * PHP License, version 3.0 or later.  A copy of this license should have
+ * been distributed with this file in the file LICENSE.PHP .  If this is not
+ * the case, you can obtain a copy at http://www.php.net/license/3_0.txt.
+ *
+ * The latest version of DOMPDF might be available at:
+ * http://www.dompdf.com/
+ *
+ * @link http://www.dompdf.com/
+ * @copyright 2004 Benj Carson
+ * @author Benj Carson <benjcarson@digitaljunkies.ca>
+ * @package dompdf
+ */
+
+/* $Id$ */
+ 
+/**
+ * DOMPDF autoload function
+ *
+ * If you have an existing autoload function, add a call to this function
+ * from your existing __autoload() implementation.
+ *
+ * @param string $class
+ */
+function DOMPDF_autoload($class) {
+  $filename = DOMPDF_INC_DIR . "/" . mb_strtolower($class) . ".cls.php";
+  
+  if ( is_file($filename) )
+    require_once($filename);
+}
+
+// If SPL autoload functions are available (PHP >= 5.1.2)
+if ( function_exists("spl_autoload_register") ) {
+  $autoload = "DOMPDF_autoload";
+  $funcs = spl_autoload_functions();
+  
+  // No functions currently in the stack. 
+  if ( !DOMPDF_AUTOLOAD_PREPEND || $funcs === false ) { 
+    spl_autoload_register($autoload); 
+  }
+  
+  // If PHP >= 5.3 the $prepend argument is available
+  else if ( version_compare(PHP_VERSION, '5.3', '>=') ) {
+    spl_autoload_register($autoload, true, true); 
+  }
+  
+  else {
+    // Unregister existing autoloaders... 
+    $compat = version_compare(PHP_VERSION, '5.1.2', '<=') && 
+              version_compare(PHP_VERSION, '5.1.0', '>=');
+              
+    foreach ($funcs as $func) { 
+      if (is_array($func)) { 
+        // :TRICKY: There are some compatibility issues and some 
+        // places where we need to error out 
+        $reflector = new ReflectionMethod($func[0], $func[1]); 
+        if (!$reflector->isStatic()) { 
+          throw new Exception('This function is not compatible with non-static object methods due to PHP Bug #44144.'); 
+        }
+        
+        // Suprisingly, spl_autoload_register supports the 
+        // Class::staticMethod callback format, although call_user_func doesn't 
+        if ($compat) $func = implode('::', $func); 
+      }
+      
+      spl_autoload_unregister($func); 
+    }
+    
+    // Register the new one, thus putting it at the front of the stack... 
+    spl_autoload_register($autoload); 
+    
+    // Now, go back and re-register all of our old ones. 
+    foreach ($funcs as $func) { 
+      spl_autoload_register($func); 
+    }
+    
+    // Be polite and ensure that userland autoload gets retained
+    if ( function_exists("__autoload") ) {
+      spl_autoload_register("__autoload");
+    }
+  }
+}
+
+else if ( !function_exists("__autoload") ) {
+  /**
+   * Default __autoload() function
+   *
+   * @param string $class
+   */
+  function __autoload($class) {
+    DOMPDF_autoload($class);
+  }
+}
