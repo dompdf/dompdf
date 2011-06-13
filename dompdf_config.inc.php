626a69b9aaa605ce127f96d76c5f44f39f423b1d
PhenX <phenxdesign@gmail.com>
2011-06-13 09:42:07 +0000

    Added PHP version checking at the beginning (5.0+). Fixes Issue 303.
    git-svn-id: http://dompdf.googlecode.com/svn/trunk/dompdf@403 8e70de24-3a84-11de-b438-597f59cd7555

--- dompdf_config.inc.php
+++ dompdf_config.inc.php
@@ -54,6 +54,8 @@
 //error_reporting(E_STRICT | E_ALL | E_DEPRECATED);
 //ini_set("display_errors", 1);
 
+PHP_VERSION >= 5.0 or die("DOMPDF requires PHP 5.0+");
+
 /**
  * The root of your DOMPDF installation
  */
