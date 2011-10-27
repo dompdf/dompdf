2055e66352cad7db451d00397b4a07922df064df
PhenX <phenxdesign@gmail.com>
2011-10-27 20:34:07 +0000

    Force to update the font cache
    git-svn-id: http://dompdf.googlecode.com/svn/trunk/dompdf@445 8e70de24-3a84-11de-b438-597f59cd7555

--- lib/class.pdf.php
+++ lib/class.pdf.php
@@ -207,7 +207,7 @@ class Cpdf {
    * @var integer The version of the font metrics cache file.
    * This value must be manually incremented whenever the internal font data structure is modified.
    */
-  public  $fontcacheVersion = 4;
+  public  $fontcacheVersion = 5;
 
   /**
    * @var string Temporary folder.
