ae3580ec0999341d5a24e3f3a719d8550f98a215
Fabien Ménager <fabien.menager@gmail.com>
2013-02-17 16:54:09 +0100

    Improve body background and border rendering (Fixes #556)

--- include/block_renderer.cls.php
+++ include/block_renderer.cls.php
@@ -27,10 +27,8 @@ class Block_Renderer extends Abstract_Renderer {
     if ( $node->nodeName === "body" ) {
       $h = $frame->get_containing_block("h") - $style->length_in_pt(array(
         $style->margin_top,
-        $style->padding_top,
         $style->border_top_width,
         $style->border_bottom_width,
-        $style->padding_bottom,
         $style->margin_bottom),
       $style->width);
     }
@@ -59,8 +57,9 @@ class Block_Renderer extends Abstract_Renderer {
       $this->_canvas->clipping_end();
     }
 
-    $this->_render_border($frame);
-    $this->_render_outline($frame);
+    $border_box = array($x, $y, $w, $h);
+    $this->_render_border($frame, $border_box);
+    $this->_render_outline($frame, $border_box);
     
     if (DEBUG_LAYOUT && DEBUG_LAYOUT_BLOCKS) {
       $this->_debug_layout($frame->get_border_box(), "red");
@@ -76,13 +75,16 @@ class Block_Renderer extends Abstract_Renderer {
     }
   }
 
-  protected function _render_border(Frame_Decorator $frame, $corner_style = "bevel") {
+  protected function _render_border(Frame_Decorator $frame, $border_box = null, $corner_style = "bevel") {
     $style = $frame->get_style();
-    $bbox = $frame->get_border_box();
     $bp = $style->get_border_properties();
+
+    if ( empty($border_box) ) {
+      $border_box = $frame->get_border_box();
+    }
     
     // find the radius
-    $radius = $style->get_computed_border_radius($bbox["w"], $bbox["h"]);
+    $radius = $style->get_computed_border_radius($border_box[2], $border_box[3]); // w, h
 
     // Short-cut: If all the borders are "solid" with the same color and style, and no radius, we'd better draw a rectangle
     if (
@@ -95,7 +97,7 @@ class Block_Renderer extends Abstract_Renderer {
       $props = $bp["top"];
       if ( $props["color"] === "transparent" || $props["width"] <= 0 ) return;
       
-      list($x, $y, $w, $h) = $bbox;
+      list($x, $y, $w, $h) = $border_box;
       $width = $style->length_in_pt($props["width"]);
       $pattern = $this->_get_dash_pattern($props["style"], $width);
       $this->_canvas->rectangle($x + $width / 2, $y + $width / 2, $w - $width, $h - $width, $props["color"], $width, $pattern);
@@ -109,7 +111,7 @@ class Block_Renderer extends Abstract_Renderer {
                     $style->length_in_pt($bp["left"]["width"]));
     
     foreach ($bp as $side => $props) {
-      list($x, $y, $w, $h) = $bbox;
+      list($x, $y, $w, $h) = $border_box;
       $length = 0;
       $r1 = 0;
       $r2 = 0;
@@ -156,7 +158,7 @@ class Block_Renderer extends Abstract_Renderer {
     }
   }
 
-  protected function _render_outline(Frame_Decorator $frame, $corner_style = "bevel") {
+  protected function _render_outline(Frame_Decorator $frame, $border_box = null, $corner_style = "bevel") {
     $style = $frame->get_style();
     
     $props = array(
@@ -167,27 +169,30 @@ class Block_Renderer extends Abstract_Renderer {
     
     if ( !$props["style"] || $props["style"] === "none" || $props["width"] <= 0 )
       return;
-      
-    $bbox = $frame->get_border_box();
+
+    if ( empty($border_box) ) {
+      $border_box = $frame->get_border_box();
+    }
+
     $offset = $style->length_in_pt($props["width"]);
     $pattern = $this->_get_dash_pattern($props["style"], $offset);
 
     // If the outline style is "solid" we'd better draw a rectangle
     if ( in_array($props["style"], array("solid", "dashed", "dotted")) ) {
-      $bbox[0] -= $offset / 2;
-      $bbox[1] -= $offset / 2;
-      $bbox[2] += $offset;
-      $bbox[3] += $offset;
+      $border_box[0] -= $offset / 2;
+      $border_box[1] -= $offset / 2;
+      $border_box[2] += $offset;
+      $border_box[3] += $offset;
     
-      list($x, $y, $w, $h) = $bbox;
+      list($x, $y, $w, $h) = $border_box;
       $this->_canvas->rectangle($x, $y, $w, $h, $props["color"], $offset, $pattern);
       return;
     }
 
-    $bbox[0] -= $offset;
-    $bbox[1] -= $offset;
-    $bbox[2] += $offset * 2;
-    $bbox[3] += $offset * 2;
+    $border_box[0] -= $offset;
+    $border_box[1] -= $offset;
+    $border_box[2] += $offset * 2;
+    $border_box[3] += $offset * 2;
     
     $method = "_border_" . $props["style"];
     $widths = array_fill(0, 4, $props["width"]);
@@ -195,7 +200,7 @@ class Block_Renderer extends Abstract_Renderer {
     $length = 0;
     
     foreach ($sides as $side) {
-      list($x, $y, $w, $h) = $bbox;
+      list($x, $y, $w, $h) = $border_box;
 
       switch($side) {
       case "top":
