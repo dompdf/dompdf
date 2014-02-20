625b29a9892efddd1df00cdd8b924e715e60905e
Gabriel Bull <me@gabrielbull.com>
2014-02-20 09:58:25 -0500

    Moved Frame classes to their own files

---
+++ include/Frame/FrameTreeList.php
@@ -0,0 +1,35 @@
+<?php
+namespace DomPdf\Frame;
+
+use IteratorAggregate;
+use Frame;
+
+/**
+ * Pre-order IteratorAggregate
+ *
+ * @access private
+ * @package dompdf
+ */
+class FrameTreeList implements IteratorAggregate
+{
+    /**
+     * @var Frame
+     */
+    protected $_root;
+
+    /**
+     * @param Frame $root
+     */
+    public function __construct(Frame $root)
+    {
+        $this->_root = $root;
+    }
+
+    /**
+     * @return FrameTreeIterator
+     */
+    public function getIterator()
+    {
+        return new FrameTreeIterator($this->_root);
+    }
+}
