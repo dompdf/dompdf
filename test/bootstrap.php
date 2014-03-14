a5e039b729e18dd9d2b95dd311618a62cf0c4bd7
Gabriel Bull <me@gabrielbull.com>
2014-03-14 10:43:29 -0400

    Fixed bug with tests bootstrap

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -3,10 +3,10 @@
 define('DOMPDF_ENABLE_AUTOLOAD', false);
 require_once __DIR__ . '/../dompdf_config.inc.php';
 
-Dompdf\Autoloader::register();
-
 if (!@include_once __DIR__ . '/../vendor/autoload.php') {
     if (!@include_once __DIR__ . '/../../../autoload.php') {
         trigger_error("Unable to load dependencies", E_USER_ERROR);
     }
 }
+
+Dompdf\Autoloader::register();
