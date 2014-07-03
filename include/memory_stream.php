259efe2d02dffa089b6a8d4e2d8d247661dc60ba
Marek Vavrečan <vavrecan@gmail.com>
2014-07-03 17:37:58 +0200

    added support for file stats

--- include/memory_stream.php
+++ include/memory_stream.php
@@ -5,8 +5,7 @@ class MemoryStream {
     private $varname;
 
     public function stream_open($path, $mode, $options, &$opened_path) {
-        $url = parse_url($path);
-        $this->varname = $url["host"];
+        $this->varname = $path;
         $this->position = 0;
         return true;
     }
@@ -50,6 +49,29 @@ class MemoryStream {
 
     public function unlink() {
         unset($GLOBALS[$this->varname]);
+    }
+
+    public function stream_stat() {
+        $stats = [];
+        $stats[0] = $stats["dev"] = 0;      // device number
+        $stats[1] = $stats["ino"] = 0;      // inode number
+        $stats[2] = $stats["mode"] = 33188; // inode protection mode - file, read, write, execute
+        $stats[3] = $stats["nlink"] = 1;    // number of links
+        $stats[4] = $stats["uid"] = 0;      // userid of owner
+        $stats[5] = $stats["gid"] = 0;      // groupid of owner
+        $stats[6] = $stats["rdev"] = 0;     // device type, if inode device
+        $stats[7] = $stats["size"] = isset($GLOBALS[$this->varname]) ? strlen($GLOBALS[$this->varname]) : 0; // 	size in bytes
+        $stats[8] = $stats["atime"] = 0;    // time of last access (Unix timestamp)
+        $stats[9] = $stats["mtime"] = 0;    // time of last modification (Unix timestamp)
+        $stats[10] = $stats["ctime"] = 0;   // time of last inode change (Unix timestamp)
+        $stats[11] = $stats["blksize"] = 4096; // blocksize of filesystem IO
+        $stats[12] = $stats["blocks"] = 1;  // number of blicks on device
+        return $stats;
+    }
+
+    public function url_stat($path, $flags) {
+        $this->varname = $path;
+        return $this->stream_stat();
     }
 }
 
