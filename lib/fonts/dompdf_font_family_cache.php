7c44c046c4086158fd3f326cce3eb95c82f1ad56
Porraphit <s55070069@kmitl.ac.th>
2015-06-05 10:15:35 +0700

    install new font 'THSarabunNew'

---
+++ lib/fonts/dompdf_font_family_cache.php
@@ -0,0 +1,118 @@
+<?php return array (
+  'sans-serif' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Helvetica',
+    'bold' => DOMPDF_FONT_DIR . 'Helvetica-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Helvetica-Oblique',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Helvetica-BoldOblique',
+  ),
+  'times' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Times-Roman',
+    'bold' => DOMPDF_FONT_DIR . 'Times-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Times-Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Times-BoldItalic',
+  ),
+  'times-roman' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Times-Roman',
+    'bold' => DOMPDF_FONT_DIR . 'Times-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Times-Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Times-BoldItalic',
+  ),
+  'courier' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Courier',
+    'bold' => DOMPDF_FONT_DIR . 'Courier-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Courier-Oblique',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Courier-BoldOblique',
+  ),
+  'helvetica' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Helvetica',
+    'bold' => DOMPDF_FONT_DIR . 'Helvetica-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Helvetica-Oblique',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Helvetica-BoldOblique',
+  ),
+  'zapfdingbats' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'ZapfDingbats',
+    'bold' => DOMPDF_FONT_DIR . 'ZapfDingbats',
+    'italic' => DOMPDF_FONT_DIR . 'ZapfDingbats',
+    'bold_italic' => DOMPDF_FONT_DIR . 'ZapfDingbats',
+  ),
+  'symbol' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Symbol',
+    'bold' => DOMPDF_FONT_DIR . 'Symbol',
+    'italic' => DOMPDF_FONT_DIR . 'Symbol',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Symbol',
+  ),
+  'serif' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Times-Roman',
+    'bold' => DOMPDF_FONT_DIR . 'Times-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Times-Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Times-BoldItalic',
+  ),
+  'monospace' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Courier',
+    'bold' => DOMPDF_FONT_DIR . 'Courier-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Courier-Oblique',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Courier-BoldOblique',
+  ),
+  'fixed' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Courier',
+    'bold' => DOMPDF_FONT_DIR . 'Courier-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Courier-Oblique',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Courier-BoldOblique',
+  ),
+  'dejavu sans' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSans-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSans-BoldOblique',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSans-Oblique',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSans',
+  ),
+  'dejavu sans light' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSans-ExtraLight',
+  ),
+  'dejavu sans condensed' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed-BoldOblique',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed-Oblique',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed',
+  ),
+  'dejavu sans mono' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSansMono-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSansMono-BoldOblique',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSansMono-Oblique',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSansMono',
+  ),
+  'dejavu serif' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSerif-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSerif-BoldItalic',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSerif-Italic',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSerif',
+  ),
+  'dejavu serif condensed' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed-BoldItalic',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed-Italic',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed',
+  ),
+  'thsarabunnew' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . '8cf74cec943c117a569b3db3731046c5',
+    'bold' => DOMPDF_FONT_DIR . '38c1d84e8a42ac39206df5416ba98503',
+    'bold_italic' => DOMPDF_FONT_DIR . '335a62f5e60b848da3516577d6dc3f84',
+    'italic' => DOMPDF_FONT_DIR . '5810d05718451e332b2d666e9f983275',
+  ),
+) ?>
