d8975060cabf667272e0d3fdf90f58cd95305ecb
bsweeney <brian@eclecticgeek.com>
2015-08-11 10:43:35 -0700

    frackin 'smart quotes' :(

--- autoload.inc.php
+++ autoload.inc.php
@@ -18,7 +18,7 @@
 
 require_once __DIR__ . '/lib/html5lib/Parser.php';
 require_once __DIR__ . '/lib/php-font-lib/src/FontLib/Autoloader.php';
-require_once __DIR__ . '/lib/php-svg-lib/src/autoload.php’;
+require_once __DIR__ . '/lib/php-svg-lib/src/autoload.php';
 
 /*
  * New PHP 5.3.0 namespaced autoloader
