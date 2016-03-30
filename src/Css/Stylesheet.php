1c471d01d3fabf45e5ccc2fa6bed3008b256e503
Andrew Scott <andrew@andrewscott.net.au>
2016-03-30 22:53:22 +1100

    Bootstrap Improvements:  * Auto-width calculation for floated and inline-block elements.  * Fix style specificity based on the origin of the stylesheet.  * Add :focus psudo-class to stylesheet parser.  * Modify default UA stylesheet to reflect auto-width changes.  * Fix 'content' css property being set to 'normal' for all :before/:after selectors before determining their specificity.  * Add extra check to prevent Helpers::build_url() from throwing a warning for URIs of 1 character (eg: '#').

--- src/Css/Stylesheet.php
+++ src/Css/Stylesheet.php
@@ -612,6 +612,7 @@ class Stylesheet
                         case "first-letter": // TODO
 
                             // N/A
+                        case "focus":
                         case "active":
                         case "hover":
                         case "visited":
@@ -860,7 +861,7 @@ class Stylesheet
                         continue;
                     }
 
-                    if (($src = $this->_image($style->content)) !== "none") {
+                    if (($src = $this->_image($style->get_prop('content'))) !== "none") {
                         $new_node = $node->ownerDocument->createElement("img_generated");
                         $new_node->setAttribute("src", $src);
                     } else {
@@ -895,7 +896,7 @@ class Stylesheet
                 $id = $node->getAttribute("frame_id");
 
                 // Assign the current style to the scratch array
-                $spec = $this->_specificity($selector);
+                $spec = $this->_specificity($selector, $style->get_origin());
                 $styles[$id][$spec][] = $style;
             }
         }
