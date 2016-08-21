dompdf/dompdf

commit 1fa43aa08ab5264b1d3713afa9ed96f886bd4638

Fix 'rem' CSS units with border-radius
Closes #1184

src/Css/Style.php
@@ -2133,7 +2133,36 @@ class Style
         //see __set and __get, on all assignments clear cache!
         $this->_prop_cache["border_" . $corner . "_radius"] = null;
 
-        $this->_props["border_" . $corner . "_radius"] = $this->length_in_pt($val);
+        $this->_props["border_" . $corner . "_radius"] = $val;
+    }
+
+    function get_border_top_left_radius()
+    {
+        return $this->_get_border_radius_corner("top_left");
+    }
+
+    function get_border_top_right_radius()
+    {
+        return $this->_get_border_radius_corner("top_right");
+    }
+
+    function get_border_bottom_left_radius()
+    {
+        return $this->_get_border_radius_corner("bottom_left");
+    }
+
+    function get_border_bottom_right_radius()
+    {
+        return $this->_get_border_radius_corner("bottom_right");
+    }
+
+    protected function _get_border_radius_corner($corner)
+    {
+        if (!isset($this->_props["border_" . $corner . "_radius"])) {
+            return;
+        }
+
+        return $this->length_in_pt($this->_props["border_" . $corner . "_radius"]);
     }
 
     /**

commit 71ae164df2b055ef22125707cf3bbb48e36c23a3

Return 0 if border radius is not set or empty
Previously we were checking for a value on the specified border
radius via `isset()`, but this condition is true for the default
value (an empty string). This resulted in the empty string being
run through `length_in_pt()`, which would then return returns the
default font size as the length.

Addresses an issue introduced by commit 1fa43aa

src/Css/Style.php
@@ -2158,8 +2158,8 @@ class Style
 
     protected function _get_border_radius_corner($corner)
     {
-        if (!isset($this->_props["border_" . $corner . "_radius"])) {
-            return;
+        if (!isset($this->_props["border_" . $corner . "_radius"]) || empty($this->_props["border_" . $corner . "_radius"])) {
+            return 0;
         }
 
         return $this->length_in_pt($this->_props["border_" . $corner . "_radius"]);