ad41b6dd17ba774b017407ffba09c14a66665723
Ed Preston <epreston@prestonsoft.com>
2017-10-09 20:29:46 +1100

    Fix: Uncaught Dompdf\Exception: No block-level parent found. Not good
    Fatal error: Uncaught Dompdf\Exception: No block-level parent found.
    Not good

--- src/Dompdf.php
+++ src/Dompdf.php
@@ -490,6 +490,16 @@ class Dompdf
             $doc->loadHTML($str);
             $doc->encoding = $encoding;
 
+            // Remove #text children nodes in nodes that shouldn't have
+            $tag_names = array("html", "table", "tbody", "thead", "tfoot", "tr");
+            foreach ($tag_names as $tag_name) {
+                $nodes = $doc->getElementsByTagName($tag_name);
+
+                foreach ($nodes as $node) {
+                    self::removeTextNodes($node);
+                }
+            }
+
             // If some text is before the doctype, we are in quirksmode
             if (preg_match("/^(.+)<!doctype/i", ltrim($str), $matches)) {
                 $quirksmode = true;
