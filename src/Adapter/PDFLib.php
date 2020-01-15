9be1e02dd49d76b89a867b3eb9754b155070d78b
Ricardo Sanz <ricardo@metadrop.net>
2020-01-15 10:44:18 +0100

    Fix get_font_height calculations based on comments from bsweeney.
    See https://github.com/dompdf/dompdf/issues/2007#issuecomment-573379582.

--- src/Adapter/PDFLib.php
+++ src/Adapter/PDFLib.php
@@ -1282,13 +1282,13 @@ class PDFLib implements Canvas
 
         $this->_pdf->setfont($fh, $size);
 
-	$asc = $this->_pdf->info_font($fh, "ascender",  "fontsize=$size") / $size;
-	$desc = $this->_pdf->info_font($fh, "descender", "fontsize=$size") / $size;
+        $asc = $this->_pdf->info_font($fh, "ascender", "fontsize=$size");
+        $desc = $this->_pdf->info_font($fh, "descender", "fontsize=$size");
 
         // $desc is usually < 0,
         $ratio = $this->_dompdf->getOptions()->getFontHeightRatio();
 
-        return $size * ($asc - $desc) * $ratio;
+        return (abs($asc) + abs($desc)) * $ratio;
     }
 
     /**
