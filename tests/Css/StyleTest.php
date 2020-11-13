d0abc0f609136fcb2933998fd9d3ab17d4e4f8e1
Brian Sweeney <brian@eclecticgeek.com>
2020-11-13 13:13:34 -0500

    Add background image regression tests

--- tests/Css/StyleTest.php
+++ tests/Css/StyleTest.php
@@ -33,4 +33,75 @@ class StyleTest extends TestCase
         $this->assertEquals(12, $length);
     }
 
+    /**
+     * @group regression
+     */
+    public function testCssImageNoneParsingNoBaseHref()
+    {
+        $dompdf = new Dompdf();
+        $sheet = new Stylesheet($dompdf);
+        $s = new Style($sheet);
+
+        // keyword none
+        $s->background_image = "none";
+        $this->assertEquals("none", $s->background_image);
+
+        // no value
+        $s->background_image = "";
+        $this->assertEquals("none", $s->background_image);
+
+        // bare url
+        $s->background_image = "http://example.com/test.png";
+        $this->assertEquals("none", $s->background_image);
+    }
+
+    /**
+     * @group regression
+     */
+    public function testCssImageNoneParsingWithBaseHref()
+    {
+        $dompdf = new Dompdf();
+        $dompdf->setProtocol("https://");
+        $dompdf->setBaseHost("example.com");
+        $dompdf->setBasePath("/");
+        $sheet = new Stylesheet($dompdf);
+        $s = new Style($sheet);
+
+        // keyword none
+        $s->background_image = "none";
+        $this->assertEquals("none", $s->background_image);
+
+        // no value
+        $s->background_image = "";
+        $this->assertEquals("none", $s->background_image);
+
+        // bare url
+        $s->background_image = "http://example.com/test.png";
+        $this->assertEquals("none", $s->background_image);
+    }
+
+    /**
+     * @group regression
+     */
+    public function testCssImageNoneParsingWithStylesheetBaseHref()
+    {
+        $dompdf = new Dompdf();
+        $sheet = new Stylesheet($dompdf);
+        $sheet->set_protocol("https://");
+        $sheet->set_host("example.com");
+        $sheet->set_base_path("/");
+        $s = new Style($sheet);
+
+        // keyword none
+        $s->background_image = "none";
+        $this->assertEquals("none", $s->background_image);
+
+        // no value
+        $s->background_image = "";
+        $this->assertEquals("none", $s->background_image);
+
+        // bare url
+        $s->background_image = "http://example.com/test.png";
+        $this->assertEquals("none", $s->background_image);
+    }
 }
