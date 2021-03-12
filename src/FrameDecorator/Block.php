96647d6c46a2727adb782c865cf67362995e1604
Till Berger <till@mellthas.de>
2021-03-12 19:20:08 +0100

    Remove frames from line boxes after split
    Fixes backgrounds or borders bleeding into the page margins at page-
    break locations. This is achieved by fixing and using the existing
    `remove_frames_from_line` method.

--- src/FrameDecorator/Block.php
+++ src/FrameDecorator/Block.php
@@ -203,50 +203,58 @@ class Block extends AbstractFrameDecorator
     }
 
     /**
+     * Remove the given frame and all following frames and lines from the block.
+     *
      * @param Frame $frame
      */
-    function remove_frames_from_line(Frame $frame)
+    public function remove_frames_from_line(Frame $frame): void
     {
-        // Search backwards through the lines for $frame
-        $i = $this->_cl;
-        $j = null;
-
-        while ($i >= 0) {
-            if (($j = in_array($frame, $this->_line_boxes[$i]->get_frames(), true)) !== false) {
-                break;
-            }
-
-            $i--;
+        // Inline frames are not added to line boxes themselves, only their
+        // text frame children
+        $actualFrame = $frame;
+        while ($actualFrame !== null && $actualFrame instanceof Inline) {
+            $actualFrame = $actualFrame->get_first_child();
         }
 
-        if ($j === false) {
+        if ($actualFrame === null) {
             return;
         }
 
-        // Remove $frame and all frames that follow
-        while ($j < count($this->_line_boxes[$i]->get_frames())) {
-            $frames = $this->_line_boxes[$i]->get_frames();
-            $f = $frames[$j];
-            $frames[$j] = null;
-            unset($frames[$j]);
-            $j++;
-            $this->_line_boxes[$i]->w -= $f->get_margin_width();
+        // Search backwards through the lines for $frame
+        $frame = $actualFrame;
+        $i = $this->_cl;
+        $j = null;
+
+        while ($i > 0) {
+            $line = $this->_line_boxes[$i];
+            foreach ($line->get_frames() as $index => $f) {
+                if ($frame === $f) {
+                    $j = $index;
+                    break 2;
+                }
+            }
+            $i--;
         }
 
-        // Recalculate the height of the line
-        $h = 0;
-        foreach ($this->_line_boxes[$i]->get_frames() as $f) {
-            $h = max($h, $f->get_margin_height());
+        if ($j === null) {
+            return;
         }
 
-        $this->_line_boxes[$i]->h = $h;
+        $line->remove_frames($j);
 
         // Remove all lines that follow
-        while ($this->_cl > $i) {
-            $this->_line_boxes[$this->_cl] = null;
-            unset($this->_line_boxes[$this->_cl]);
-            $this->_cl--;
+        for ($k = $this->_cl; $k > $i; $k--) {
+            unset($this->_line_boxes[$k]);
         }
+
+        // Remove the line, if it is empty
+        if ($j === 0) {
+            unset($this->_line_boxes[$i]);
+        }
+
+        // Reset array indices
+        $this->_line_boxes = array_values($this->_line_boxes);
+        $this->_cl = count($this->_line_boxes) - 1;
     }
 
     /**
