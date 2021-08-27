7a071346d43ea9d597162bcdbe3e6eb3dac10d2f
Jáchym Toušek <enumag@gmail.com>
2021-08-27 03:10:21 +0200

    Fix undefined variable (#2466)
    * Bump version to 0.8.6
    
    * Reset version string to commit hash
    
    * Bump version to 1.0.1
    
    * Reset version string to commit hash
    
    * Bump version to 1.0.2
    
    * Reset version string to commit hash
    
    * Fix undefined variable
    
    * Fix indentation
    
    * Fix undefined variable
    
    * Update src/FontMetrics.php
    
    * Update src/FontMetrics.php
    
    Co-authored-by: Brian Sweeney <brian@eclecticgeek.com>

--- src/FontMetrics.php
+++ src/FontMetrics.php
@@ -92,7 +92,8 @@ class FontMetrics
     public function saveFontFamilies()
     {
         // replace the path to the DOMPDF font directories with the corresponding constants (allows for more portability)
-        $cacheData = sprintf("<?php return array (%s", PHP_EOL);
+        $cacheData = sprintf("<?php return function (%s, %s) {%s", '$fontDir', '$rootDir', PHP_EOL);
+        $cacheData .= sprintf("return array (%s", PHP_EOL);
         foreach ($this->fontLookup as $family => $variants) {
             $cacheData .= sprintf("  '%s' => array(%s", addslashes($family), PHP_EOL);
             foreach ($variants as $variant => $path) {
@@ -103,7 +104,8 @@ class FontMetrics
             }
             $cacheData .= sprintf("  ),%s", PHP_EOL);
         }
-        $cacheData .= ") ?>";
+        $cacheData .= ");" . PHP_EOL;
+        $cacheData .= "}; ?>";
         file_put_contents($this->getCacheFile(), $cacheData);
     }
 
@@ -130,14 +132,16 @@ class FontMetrics
         if (!defined("DOMPDF_FONT_DIR")) { define("DOMPDF_FONT_DIR", $fontDir); }
 
         $file = $rootDir . "/lib/fonts/dompdf_font_family_cache.dist.php";
-        $distFonts = require $file;
+        $distFontsClosure = require $file;
+        $distFonts = $distFontsClosure($rootDir);
 
         if (!is_readable($this->getCacheFile())) {
             $this->fontLookup = $distFonts;
             return;
         }
 
-        $cacheData = require $this->getCacheFile();
+        $cacheDataClosure = require $this->getCacheFile();
+        $cacheData = is_array($cacheDataClosure) ? $cacheDataClosure : $cacheDataClosure($fontDir, $rootDir);
 
         $this->fontLookup = [];
         if (is_array($this->fontLookup)) {
@@ -195,7 +199,7 @@ class FontMetrics
         mb_substitute_character($substchar);
         $prefix = preg_replace("[\W]", "_", $prefix);
         $prefix = preg_replace("/[^-_\w]+/", "", $prefix);
-        
+
         $localFile = $fontDir . "/" . $prefix . "_" . $remoteHash;
 
         if (isset($entry[$styleString]) && $localFile == $entry[$styleString]) {
