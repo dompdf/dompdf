dd5389ed77f76f4f1b53fcc0f131a064454d255e
Till Berger <till@mellthas.de>
2021-12-09 01:23:11 +0100

    Add basic test for `page_script`/`page_text`/`page_line` methods

---
+++ tests/Canvas/CPDFTest.php
@@ -0,0 +1,52 @@
+<?php
+namespace Dompdf\Tests\Canvas;
+
+use Dompdf\Adapter\CPDF;
+use Dompdf\Canvas;
+use Dompdf\Dompdf;
+use Dompdf\FontMetrics;
+use Dompdf\Options;
+use Dompdf\Tests\TestCase;
+
+class CPDFTest extends TestCase
+{
+    public function testPageScript(): void
+    {
+        global $called;
+        $called = 0;
+
+        $options = new Options();
+        $options->setIsPhpEnabled(true);
+        $dompdf = new Dompdf($options);
+
+        $canvas = new CPDF([0, 0, 200, 200], "portrait", $dompdf);
+        $canvas->new_page();
+
+        $canvas->page_script(function (
+            int $pageNumber,
+            int $pageCount,
+            Canvas $canvas,
+            FontMetrics $fontMetrics
+        ) use (&$called) {
+            $called++;
+            $font = $fontMetrics->getFont("Helvetica");
+            $canvas->text(40, 20, "Page $pageNumber of $pageCount", $font, 12);
+            $canvas->line(200, 0, 0, 200, [0, 0, 0], 1);
+        });
+        $canvas->page_script('
+            global $called;
+            $called++;
+            $font = $fontMetrics->getFont("Helvetica");
+            $pdf->text(20, 0, "Page $PAGE_NUM of $PAGE_COUNT", $font, 12);
+        ');
+
+        $font = $dompdf->getFontMetrics()->getFont("Helvetica");
+        $canvas->page_text(60, 40, "Page {PAGE_NUM} of {PAGE_COUNT}", $font, 12);
+        $canvas->page_line(0, 0, 200, 200, [0, 0, 0], 1);
+
+        $output = $canvas->output();
+
+        $this->assertNotSame("", $output);
+        $this->assertSame(4, $called);
+    }
+}
