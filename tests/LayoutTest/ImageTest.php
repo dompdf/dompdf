dfca72b5c50bce9f81c4c3cf704cbd5837cf7e7d
Till Berger <till@mellthas.de>
2021-12-20 15:24:22 +0100

    Add basic layout tests for resolving image dimensions
    As an alternative to the existing FrameReflower tests.
    
    Differences in expected sizes for most of the tests is due to the
    FrameReflower tests not mocking DPI for the Style class, so calculations
    via `length_in_pt` use the default 96 DPI, while `auto` image
    dimensions are resolved with the mocked 75 DPI.
    
    The layout tests use the default DPI instead to avoid failing tests due
    to static caches (in `length_in_pt`) when running the entire test suite
    at once. This could be solved with the `@runTestsInSeparateProcesses`/
    `@runInSeparateProcess` annotations, but that makes testing noticeably
    slower.

---
+++ tests/LayoutTest/ImageTest.php
@@ -0,0 +1,170 @@
+<?php
+namespace Dompdf\Tests\LayoutTest;
+
+use Dompdf\Dompdf;
+use Dompdf\FrameDecorator\AbstractFrameDecorator;
+use Dompdf\Options;
+use Dompdf\Tests\TestCase;
+
+class ImageTest extends TestCase
+{
+    public function imageDimensionsProvider(): array
+    {
+        $filepath = "../_files/jamaica.jpg";
+        $dpiFactor = 72 / 96;
+        $intrinsicWidth = 2048 * $dpiFactor;
+        $intrinsicHeight = 1536 * $dpiFactor;
+
+        return [
+            // TODO: Heredocs can be nicely indented starting with PHP 7.3
+            "zero" => [
+                <<<HTML
+<img src="$filepath" style="width: 0; height: 0;">
+HTML
+,
+                0.0,
+                0.0
+            ],
+            "auto" => [
+                <<<HTML
+<img src="$filepath">
+HTML
+,
+                $intrinsicWidth,
+                $intrinsicHeight
+            ],
+            "fixed px" => [
+                <<<HTML
+<img src="$filepath" width="100" height="200">
+HTML
+,
+                100 * $dpiFactor,
+                200 * $dpiFactor
+            ],
+            "fixed pt" => [
+                <<<HTML
+<img src="$filepath" style="width: 100pt; height: 200pt;">
+HTML
+,
+                100.0,
+                200.0
+            ],
+            "min-max" => [
+                <<<HTML
+<img src="$filepath" style="
+    width: 100px;
+    height: 1200px;
+    min-width: 400px;
+    max-width: 800px;
+    min-height: 300px;
+    max-height: 500px;
+    ">
+HTML
+,
+                400 * $dpiFactor,
+                500 * $dpiFactor
+            ],
+            "page size" => [
+                <<<HTML
+<img src="$filepath" style="width: 100%; height: 100%;">
+HTML
+,
+                400.0,
+                300.0
+            ],
+            "percentage chain" => [
+                <<<HTML
+<div style="width: 400px; height: 800px;">
+    <div style="width: 50%; height: 75%;">
+        <img src="$filepath" style="width: 50%; height: 75%;">
+    </div>
+</div>
+HTML
+,
+                100 * $dpiFactor,
+                450 * $dpiFactor
+            ],
+            "in table" => [
+                <<<HTML
+<table style="width: 20%; border-collapse: collapse;">
+    <tr>
+        <td style="padding: 0;">
+            <img src="$filepath">
+        </td>
+    </tr>
+</div>
+HTML
+,
+                $intrinsicWidth,
+                $intrinsicHeight
+            ],
+            "in table max-width" => [
+                <<<HTML
+<table style="width: 20%; border-collapse: collapse;">
+    <tr>
+        <td style="padding: 0;">
+            <img src="$filepath" style="max-width: 100%; height: auto;">
+        </td>
+    </tr>
+</div>
+HTML
+,
+                80.0,
+                60.0
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider imageDimensionsProvider
+     */
+    public function testImageDimensions(
+        string $body,
+        float $expectedWidth,
+        float $expectedHeight
+    ): void {
+        $width = null;
+        $height = null;
+
+        $options = new Options();
+        $options->setLogOutputFile("");
+
+        // Use callback to inspect frame tree
+        $dompdf = new Dompdf($options);
+        $dompdf->setBasePath(__DIR__);
+        $dompdf->setCallbacks([
+            [
+                "event" => "begin_frame",
+                "f" => function ($info) use (&$width, &$height) {
+                    /** @var AbstractFrameDecorator */
+                    $frame = $info["frame"];
+
+                    if ($frame->get_node()->nodeName === "img") {
+                        [, , $width, $height] = $frame->get_content_box();
+                    }
+                }
+            ]
+        ]);
+
+        $dompdf->loadHtml(<<<HTML
+    <!DOCTYPE html>
+    <head>
+    <meta charset="UTF-8">
+    <style>
+        @page {
+            size: 400pt 300pt;
+            margin: 0;
+        }
+    </style>
+    </head>
+    <html>
+    <body>$body</body>
+    </html>
+HTML
+        );
+        $dompdf->render();
+
+        $this->assertSame($expectedWidth, $width);
+        $this->assertSame($expectedHeight, $height);
+    }
+}
