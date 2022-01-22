dompdf/dompdf

commit 2d0af0ca3350fe5b20a6b8040cfef8ef274a704a

Fix reference size for vertical margins and padding
Top and bottom margins and padding are relative to the width of the
containing block. This is already set properly in most places, but some
places used the height. For the page margins though, the reference size
is the page height.

The bottom-spacing calculation for page breaks had no reference size
set, so the font size was used, and percentage margins could lead to
content bleeding into the page margins.

Didn’t touch most of the table code and fixed positioning for now, as
these probably require more testing.

https://www.w3.org/TR/CSS21/box.html#margin-properties
https://www.w3.org/TR/CSS21/box.html#padding-properties
https://www.w3.org/TR/CSS21/page.html#page-margins

src/FrameDecorator/Page.php
@@ -20,13 +20,14 @@ use Dompdf\Renderer;
  */
 class Page extends AbstractFrameDecorator
 {
-
     /**
-     * y value of bottom page margin
+     * The y value of the bottom edge of the page area.
+     *
+     * https://www.w3.org/TR/CSS21/page.html#page-margins
      *
      * @var float
      */
-    protected $_bottom_page_margin;
+    protected $bottom_page_edge;
 
     /**
      * Flag indicating page is full.
@@ -69,7 +70,7 @@ class Page extends AbstractFrameDecorator
         parent::__construct($frame, $dompdf);
         $this->_page_full = false;
         $this->_in_table = 0;
-        $this->_bottom_page_margin = null;
+        $this->bottom_page_edge = null;
     }
 
     /**
@@ -93,7 +94,7 @@ class Page extends AbstractFrameDecorator
     }
 
     /**
-     * Set the frame's containing block.  Overridden to set $this->_bottom_page_margin.
+     * Set the frame's containing block.  Overridden to set $this->bottom_page_edge.
      *
      * @param float $x
      * @param float $y
@@ -103,10 +104,12 @@ class Page extends AbstractFrameDecorator
     function set_containing_block($x = null, $y = null, $w = null, $h = null)
     {
         parent::set_containing_block($x, $y, $w, $h);
-        //$w = $this->get_containing_block("w");
+
         if (isset($h)) {
-            $this->_bottom_page_margin = $h;
-        } // - $this->_frame->get_style()->length_in_pt($this->_frame->get_style()->margin_bottom, $w);
+            $style = $this->get_style();
+            $margin_bottom = $style->length_in_pt($style->margin_bottom, $h);
+            $this->bottom_page_edge = $h - $margin_bottom;
+        }
     }
 
     /**
@@ -543,13 +546,14 @@ class Page extends AbstractFrameDecorator
         // If a split is to occur here, then the bottom margins & paddings of all
         // parents of $frame must fit on the page as well:
         $p = $frame->get_parent();
-        while ($p) {
-            $max_y += (float) $p->get_style()->computed_bottom_spacing();
+        while ($p && $p !== $this) {
+            $cbw = $p->get_containing_block("w");
+            $max_y += (float) $p->get_style()->computed_bottom_spacing($cbw);
             $p = $p->get_parent();
         }
 
         // Check if $frame flows off the page
-        if ($max_y <= $this->_bottom_page_margin) {
+        if ($max_y <= $this->bottom_page_edge) {
             // no: do nothing
             return false;
         }

src/Css/Style.php
@@ -1075,10 +1075,15 @@ class Style
     }
 
     /**
+     * @param float $cbw The width of the containing block.
      * @return float|null|string
      */
-    function computed_bottom_spacing()
+    function computed_bottom_spacing(float $cbw)
     {
+        // Caching the bottom spacing independently of the given width is a bit
+        // iffy, but should be okay, as the containing block should only
+        // potentially change after a page break, and the style is reset in that
+        // case
         if ($this->_computed_bottom_spacing !== null) {
             return $this->_computed_bottom_spacing;
         }
@@ -1087,7 +1092,8 @@ class Style
                 $this->margin_bottom,
                 $this->padding_bottom,
                 $this->border_bottom_width
-            ]
+            ],
+            $cbw
         );
     }
 

src/FrameReflower/AbstractFrameReflower.php
@@ -84,8 +84,8 @@ abstract class AbstractFrameReflower
         $cb = $frame->get_containing_block();
         $style = $frame->get_style();
 
-        $t = $style->length_in_pt($style->margin_top, $cb["h"]);
-        $b = $style->length_in_pt($style->margin_bottom, $cb["h"]);
+        $t = $style->length_in_pt($style->margin_top, $cb["w"]);
+        $b = $style->length_in_pt($style->margin_bottom, $cb["w"]);
 
         // Handle 'auto' values
         if ($t === "auto") {
@@ -115,7 +115,7 @@ abstract class AbstractFrameReflower
 
         if ($n) {
             $n_style = $n->get_style();
-            $n_t = (float)$n_style->length_in_pt($n_style->margin_top, $cb["h"]);
+            $n_t = (float)$n_style->length_in_pt($n_style->margin_top, $cb["w"]);
 
             $b = $this->_get_collapsed_margin_length($b, $n_t);
             $style->margin_bottom = $b;
@@ -141,7 +141,7 @@ abstract class AbstractFrameReflower
             // Margins are collapsed only between block-level boxes
             if ($f) {
                 $f_style = $f->get_style();
-                $f_t = (float)$f_style->length_in_pt($f_style->margin_top, $cb["h"]);
+                $f_t = (float)$f_style->length_in_pt($f_style->margin_top, $cb["w"]);
 
                 $t = $this->_get_collapsed_margin_length($t, $f_t);
                 $style->margin_top = $t;
@@ -168,7 +168,7 @@ abstract class AbstractFrameReflower
             // Margins are collapsed only between block-level boxes
             if ($l) {
                 $l_style = $l->get_style();
-                $l_b = (float)$l_style->length_in_pt($l_style->margin_bottom, $cb["h"]);
+                $l_b = (float)$l_style->length_in_pt($l_style->margin_bottom, $cb["w"]);
 
                 $b = $this->_get_collapsed_margin_length($b, $l_b);
                 $style->margin_bottom = $b;

src/FrameReflower/Block.php
@@ -295,24 +295,29 @@ class Block extends AbstractFrameReflower
         $top = $style->length_in_pt($style->top, $cb["h"]);
         $bottom = $style->length_in_pt($style->bottom, $cb["h"]);
 
-        $margin_top = $style->length_in_pt($style->margin_top, $cb["h"]);
-        $margin_bottom = $style->length_in_pt($style->margin_bottom, $cb["h"]);
+        $margin_top = $style->length_in_pt($style->margin_top, $cb["w"]);
+        $margin_bottom = $style->length_in_pt($style->margin_bottom, $cb["w"]);
 
         if ($frame->is_absolute()) {
 
             // see http://www.w3.org/TR/CSS21/visudet.html#abs-non-replaced-height
 
-            $dims = [$top !== "auto" ? $top : 0,
+            $h_dims = [
+                $top !== "auto" ? $top : 0,
+                $height !== "auto" ? $height : 0,
+                $bottom !== "auto" ? $bottom : 0
+            ];
+            $w_dims = [
                 $style->margin_top !== "auto" ? $style->margin_top : 0,
                 $style->padding_top,
                 $style->border_top_width,
-                $height !== "auto" ? $height : 0,
                 $style->border_bottom_width,
                 $style->padding_bottom,
-                $style->margin_bottom !== "auto" ? $style->margin_bottom : 0,
-                $bottom !== "auto" ? $bottom : 0];
+                $style->margin_bottom !== "auto" ? $style->margin_bottom : 0
+            ];
 
-            $sum = (float)$style->length_in_pt($dims, $cb["h"]);
+            $sum = (float)$style->length_in_pt($h_dims, $cb["h"])
+                + (float)$style->length_in_pt($w_dims, $cb["w"]);
 
             $diff = $cb["h"] - $sum;
 
@@ -834,11 +839,11 @@ class Block extends AbstractFrameReflower
         // Determine the content edge
         $top = (float)$style->length_in_pt([$style->margin_top,
             $style->padding_top,
-            $style->border_top_width], $cb["h"]);
+            $style->border_top_width], $cb["w"]);
 
         $bottom = (float)$style->length_in_pt([$style->border_bottom_width,
             $style->margin_bottom,
-            $style->padding_bottom], $cb["h"]);
+            $style->padding_bottom], $cb["w"]);
 
         $cb_x = $x + (float)$left_margin + (float)$style->length_in_pt([$style->border_left_width,
                 $style->padding_left], $cb["w"]);

src/FrameReflower/Table.php
@@ -459,7 +459,7 @@ class Table extends AbstractFrameReflower
                 $style->border_left_width], $cb["w"]);
         $content_y = $y + (float)$style->length_in_pt([$style->margin_top,
                 $style->border_top_width,
-                $style->padding_top], $cb["h"]);
+                $style->padding_top], $cb["w"]);
 
         if (isset($cb["h"])) {
             $h = $cb["h"];
@@ -487,7 +487,7 @@ class Table extends AbstractFrameReflower
             $child->reflow();
 
             if (!$page->in_nested_table()) {
-                // Check if a split has occured
+                // Check if a split has occurred
                 $page->check_page_break($child);
             }
 
@@ -522,7 +522,7 @@ class Table extends AbstractFrameReflower
 
         $this->_frame->normalise();
 
-        // Add the cells to the cellmap (this will calcluate column widths as
+        // Add the cells to the cellmap (this will calculate column widths as
         // frames are added)
         $this->_frame->get_cellmap()->add_frame($this->_frame);
 

commit 0325adfb8831ef4031bb84bb7322a28104b92fe1

Fix page-break determination with differing page margins
This was broken by commit 2d0af0ca3350fe5b20a6b8040cfef8ef274a704a.

Fixes #2758

src/FrameDecorator/Page.php
@@ -94,22 +94,16 @@ class Page extends AbstractFrameDecorator
     }
 
     /**
-     * Set the frame's containing block.  Overridden to set $this->bottom_page_edge.
-     *
-     * @param float $x
-     * @param float $y
-     * @param float $w
-     * @param float $h
+     * Calculate the bottom edge of the page area after margins have been
+     * applied for the current page.
      */
-    function set_containing_block($x = null, $y = null, $w = null, $h = null)
+    public function calculate_bottom_page_edge(): void
     {
-        parent::set_containing_block($x, $y, $w, $h);
+        [, , , $cbh] = $this->get_containing_block();
+        $style = $this->get_style();
+        $margin_bottom = (float) $style->length_in_pt($style->margin_bottom, $cbh);
 
-        if (isset($h)) {
-            $style = $this->get_style();
-            $margin_bottom = (float) $style->length_in_pt($style->margin_bottom, $h);
-            $this->bottom_page_edge = $h - $margin_bottom;
-        }
+        $this->bottom_page_edge = $cbh - $margin_bottom;
     }
 
     /**

src/FrameReflower/Page.php
@@ -44,8 +44,8 @@ class Page extends AbstractFrameReflower
     }
 
     /**
-     * @param Frame $frame
-     * @param $page_number
+     * @param PageFrameDecorator $frame
+     * @param int $page_number
      */
     function apply_page_style(Frame $frame, $page_number)
     {
@@ -83,6 +83,8 @@ class Page extends AbstractFrameReflower
 
             $frame->set_style($style);
         }
+
+        $frame->calculate_bottom_page_edge();
     }
 
     /**
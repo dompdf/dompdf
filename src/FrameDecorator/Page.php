0325adfb8831ef4031bb84bb7322a28104b92fe1
Till Berger <till@mellthas.de>
2022-01-22 16:36:10 +0100

    Fix page-break determination with differing page margins
    This was broken by commit 2d0af0ca3350fe5b20a6b8040cfef8ef274a704a.
    
    Fixes #2758

--- src/FrameDecorator/Page.php
+++ src/FrameDecorator/Page.php
@@ -94,22 +94,16 @@ class Page extends AbstractFrameDecorator
     }
 
     /**
-     * Set the frame's containing block.  Overridden to set $this->bottom_page_edge.
-     *
-     * @param float $x
-     * @param float $y
-     * @param float $w
-     * @param float $h
+     * Calculate the bottom edge of the page area after margins have been
+     * applied for the current page.
      */
-    function set_containing_block($x = null, $y = null, $w = null, $h = null)
+    public function calculate_bottom_page_edge(): void
     {
-        parent::set_containing_block($x, $y, $w, $h);
+        [, , , $cbh] = $this->get_containing_block();
+        $style = $this->get_style();
+        $margin_bottom = (float) $style->length_in_pt($style->margin_bottom, $cbh);
 
-        if (isset($h)) {
-            $style = $this->get_style();
-            $margin_bottom = (float) $style->length_in_pt($style->margin_bottom, $h);
-            $this->bottom_page_edge = $h - $margin_bottom;
-        }
+        $this->bottom_page_edge = $cbh - $margin_bottom;
     }
 
     /**
