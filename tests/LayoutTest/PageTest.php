63d0586d675f8d9f73d1f56702151e123ffa5059
Till Berger <till@mellthas.de>
2022-01-22 16:41:05 +0100

    Add basic tests for page breaks/styles
    This is mostly exploration on how to test layout assertions. Having
    each test case define a full document makes it easy to copy and render
    the document for manual testing, e.g. if it is unclear why a test case
    fails. Having the doctype defined makes sure that quirks mode does not
    affect rendering when viewing the test case in a browser.

---
+++ tests/LayoutTest/PageTest.php
@@ -0,0 +1,129 @@
+<?php
+namespace Dompdf\Tests\LayoutTest;
+
+use DOMElement;
+use Dompdf\Canvas;
+use Dompdf\Dompdf;
+use Dompdf\FrameDecorator\AbstractFrameDecorator;
+use Dompdf\Options;
+use Dompdf\Tests\TestCase;
+
+class PageTest extends TestCase
+{
+    public function pageBreakProvider(): array
+    {
+        return [
+            // TODO: Heredocs can be nicely indented starting with PHP 7.3
+            "one page" => [
+                <<<HTML
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<style>
+    @page {
+        size: 400pt 400pt;
+        margin: 0;
+    }
+
+    body {
+        background-color: rgb(0, 0, 0, 0.05);
+    }
+
+    .box {
+        height: 400pt;
+        background-color: lightblue;
+    }
+</style>
+</head>
+<body><div class="box"></div></body>
+</html>
+HTML
+,
+                1,
+                ["box" => 1]
+            ],
+            "two pages" => [
+                <<<HTML
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<style>
+    @page {
+        size: 400pt 400pt;
+        margin: 0;
+    }
+
+    @page :first {
+        margin-bottom: 100pt;
+    }
+
+    body {
+        background-color: rgb(0, 0, 0, 0.05);
+    }
+
+    .box {
+        height: 400pt;
+        background-color: lightblue;
+    }
+</style>
+</head>
+<body><div class="box"></div></body>
+</html>
+HTML
+,
+                2,
+                ["box" => 2]
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider pageBreakProvider
+     */
+    public function testPageBreak(
+        string $html,
+        int $pageCount,
+        array $expectedPages
+    ): void {
+        $elementPages = [];
+
+        $options = new Options();
+        $options->setLogOutputFile("");
+
+        // Use callback to inspect frame tree
+        $dompdf = new Dompdf($options);
+        $dompdf->setCallbacks([
+            [
+                "event" => "begin_frame",
+                "f" => function ($info) use ($expectedPages, &$elementPages) {
+                    /** @var Canvas */
+                    $canvas = $info["canvas"];
+                    /** @var AbstractFrameDecorator */
+                    $frame = $info["frame"];
+                    $node = $frame->get_node();
+
+                    if (!($node instanceof DOMElement)) {
+                        return;
+                    }
+
+                    $class = $node->getAttribute("class");
+
+                    if (isset($expectedPages[$class])) {
+                        $elementPages[$class] = $canvas->get_page_number();
+                    }
+                }
+            ]
+        ]);
+
+        $dompdf->loadHtml($html);
+        $dompdf->render();
+
+        $this->assertSame($pageCount, $dompdf->getCanvas()->get_page_count());
+
+        foreach ($expectedPages as $class => $pageNumber) {
+            $this->assertSame($pageNumber, $elementPages[$class] ?? 0);
+        }
+    }
+}
