440b742e396a49b2b97a103949201c1eaf642d9b
Till Berger <till@mellthas.de>
2023-11-04 16:18:47 +0100

    Account for out-of-flow children during inline rendering

--- src/Renderer/Inline.php
+++ src/Renderer/Inline.php
@@ -17,23 +17,27 @@ class Inline extends AbstractRenderer
 {
     function render(Frame $frame)
     {
-        if (!$frame->get_first_child()) {
+        // Get the first in-flow child
+        $child = $frame->get_first_child();
+        while ($child && !$child->is_in_flow()) {
+            $child = $child->get_next_sibling();
+        }
+
+        if (!$child) {
             return; // No children, no service
         }
 
         $style = $frame->get_style();
         $node = $frame->get_node();
-        $dompdf = $this->_dompdf;
 
         $this->_set_opacity($frame->get_opacity($style->opacity));
 
-        $do_debug_layout_line = $dompdf->getOptions()->getDebugLayout()
-            && $dompdf->getOptions()->getDebugLayoutInline();
-
-        // Draw the background & border behind each child.  To do this we need
-        // to figure out just how much space each child takes:
-        [$x, $y] = $frame->get_first_child()->get_position();
-        [$w, $h] = $this->get_child_size($frame, $do_debug_layout_line);
+        // Draw background & border behind each child. To do this, we need to
+        // to figure out just how much space each child takes. Retrieve the
+        // position of the first child again, to account for text and vertical
+        // alignment
+        [$x, $y] = $child->get_position();
+        [$w, $h] = $this->get_child_size($frame);
 
         [, , $cbw] = $frame->get_containing_block();
         $margin_left = $style->length_in_pt($style->margin_left, $cbw);
@@ -57,15 +61,38 @@ class Inline extends AbstractRenderer
 
         $this->addNamedDest($node);
         $this->addHyperlink($node, $border_box);
+
+        $options = $this->_dompdf->getOptions();
+
+        if ($options->getDebugLayout() && $options->getDebugLayoutInline()) {
+            $this->debugLayout($border_box, "blue");
+
+            if ($options->getDebugLayoutPaddingBox()) {
+                $padding_box = [
+                    $x + $style->border_left_width,
+                    $y + $style->border_top_width,
+                    $w - $style->border_left_width - $style->border_right_width,
+                    $h - $style->border_top_width - $style->border_bottom_width
+                ];
+                $this->debugLayout($padding_box, "blue", [0.5, 0.5]);
+            }
+        }
     }
 
-    protected function get_child_size(Frame $frame, bool $do_debug_layout_line): array
+    protected function get_child_size(Frame $frame): array
     {
         $w = 0.0;
         $h = 0.0;
 
         foreach ($frame->get_children() as $child) {
-            if ($child->get_node()->nodeValue === " " && $child->get_prev_sibling() && !$child->get_next_sibling()) {
+            if (!$child->is_in_flow()) {
+                continue;
+            }
+
+            // Exclude trailing white space
+            if ($child->get_node()->nodeValue === " "
+                && $child->get_prev_sibling() && !$child->get_next_sibling()
+            ) {
                 break;
             }
 
@@ -75,7 +102,7 @@ class Inline extends AbstractRenderer
             [, , $child_w, $child_h] = $child->get_padding_box();
 
             if ($auto_width || $auto_height) {
-                [$child_w2, $child_h2] = $this->get_child_size($child, $do_debug_layout_line);
+                [$child_w2, $child_h2] = $this->get_child_size($child);
 
                 if ($auto_width) {
                     $child_w = $child_w2;
@@ -88,14 +115,6 @@ class Inline extends AbstractRenderer
 
             $w += $child_w;
             $h = max($h, $child_h);
-
-            if ($do_debug_layout_line) {
-                $this->debugLayout($child->get_border_box(), "blue");
-
-                if ($this->_dompdf->getOptions()->getDebugLayoutPaddingBox()) {
-                    $this->debugLayout($child->get_padding_box(), "blue", [0.5, 0.5]);
-                }
-            }
         }
 
         return [$w, $h];
