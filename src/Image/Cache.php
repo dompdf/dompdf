2538424f0512030db29804d8c865956d40dfc99d
Allon Moritz <allon.moritz@digital-peak.com>
2024-12-10 08:09:57 +0100

    xml_set_*_handler() functions must be callable

--- src/Image/Cache.php
+++ src/Image/Cache.php
@@ -168,7 +168,7 @@ class Cache
                             }
                         }
                     },
-                    false
+                    function () {}
                 );
         
                 if (($fp = fopen($resolved_url, "r")) !== false) {
