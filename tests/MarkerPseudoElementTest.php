6a46915b3f25e255d346d79ee6ecb1380b94e79a
Ronald Fiering <rfiering@googlemail.com>
2026-09-03 15:46:11 +0200

    Test nested list-item marker counters

--- tests/MarkerPseudoElementTest.php
+++ tests/MarkerPseudoElementTest.php
@@ -2,9 +2,12 @@
 namespace Dompdf\Tests;
 
 use Dompdf\Css\Content\Counter;
+use Dompdf\Css\Content\Counters;
 use Dompdf\Css\Content\StringPart;
 use Dompdf\Dompdf;
 use Dompdf\FrameDecorator\AbstractFrameDecorator;
+use Dompdf\Renderer\ListBullet as ListBulletRenderer;
+use ReflectionMethod;
 
 final class MarkerPseudoElementTest extends TestCase
 {
@@ -70,5 +73,75 @@ HTML
             $this->assertInstanceOf(StringPart::class, $marker["content"][2]);
             $this->assertSame(") ", $marker["content"][2]->string);
         }
+    }
+
+    public function testNestedListItemCountersAreResolved(): void
+    {
+        $markers = [];
+
+        $dompdf = new Dompdf();
+        $renderer = new ListBulletRenderer($dompdf);
+        $resolver = new ReflectionMethod(ListBulletRenderer::class, "resolve_marker_content");
+        $resolver->setAccessible(true);
+
+        $dompdf->setCallbacks([
+            [
+                "event" => "begin_frame",
+                "f" => function (AbstractFrameDecorator $frame) use (&$markers, $renderer, $resolver): void {
+                    $node = $frame->get_node();
+                    if ($node->nodeName !== "bullet") {
+                        return;
+                    }
+
+                    $content = $frame->get_style()->content;
+                    if (!is_array($content) || !isset($content[0]) || !$content[0] instanceof Counters) {
+                        return;
+                    }
+
+                    $index = $node->hasAttribute("dompdf-counter")
+                        ? (int) $node->getAttribute("dompdf-counter")
+                        : 0;
+
+                    $markers[] = $resolver->invoke($renderer, $frame, $index);
+                },
+            ],
+        ]);
+
+        $dompdf->loadHtml(<<<HTML
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ol.legal-list {
+    list-style: none;
+}
+
+ol.legal-list > li::marker {
+    content: counters(list-item, ".") " ";
+}
+</style>
+</head>
+<body>
+<ol class="legal-list">
+    <li>Outer
+        <ol class="legal-list">
+            <li>First</li>
+            <li>Second</li>
+            <li>Third</li>
+        </ol>
+    </li>
+</ol>
+</body>
+</html>
+HTML
+        );
+        $dompdf->render();
+
+        $this->assertSame([
+            "1 ",
+            "1.1 ",
+            "1.2 ",
+            "1.3 ",
+        ], $markers);
     }
 }
